b1bd9bd6974498d093ad1a4c5b4c776027a17881
HarshDubey1011 <pandetahallu@gmail.com>
2025-05-16 15:22:42 +0530

    2.2: Course information step 7

--- part2/course-information/src/components/Course.jsx
+++ part2/course-information/src/components/Course.jsx
@@ -14,17 +14,28 @@ const Part = (props) => (
   </p>
 )
 
+const SumExcercises = (props) => {
+    return(
+        <p>total of {props.sum} exercises</p>
+    );
+}
 
 
 const Course = ({course}) => {
+    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       <Header course={course.name} />
       <ul>
         {course.parts.map(part=>
             <Content key={part.id} parts={part} />
+            
         )}
       </ul>
+      {course.parts.reduce((part)=>
+        <SumExcercises sum={totalExercises} />
+      ,0)}
     </div>
   )
 }
